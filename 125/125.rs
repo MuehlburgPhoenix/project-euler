65d535f7104084f8e463eef6faed43e896082660
Matthias Drexler <mdrexler@posteo.de>
2020-03-14 17:53:42 +0100

    Approach problem 125 constructively

--- 125/125.rs
+++ 125/125.rs
@@ -1,14 +1,54 @@
+use std::collections::HashSet;
 use std::ops::RangeInclusive;
 
 fn main() {
+    find_palindromic_numbers_with_consecutive_squares_constructively(10000000);
+    find_palindromic_numbers_with_consecutive_squares_by_number(10000000);
+}
+
+fn find_palindromic_numbers_with_consecutive_squares_constructively(excluding_upper_limit: i64) {
+    let mut sum: i128 = 0;
+    let mut found_numbers: HashSet<i64> = HashSet::new();
+    let highest_possible_square = (excluding_upper_limit as f64).sqrt() as i64;
+
+    // Non-including ranges, since at least two consecutive squares needed
+    for current_range_start in 1 .. highest_possible_square {
+        for current_range_end in current_range_start + 1 .. highest_possible_square {
+            let mut retrieved_number: i64 = 0;
+            for number in current_range_start ..= current_range_end {
+                retrieved_number += number * number;
+            }
+
+            // Contain retrieved number if palindromic and in limit as well as not retrieved yet
+            // (p.e. 554455 by 331..=335 and 9..=118 and 9343439 by 102..=307 and 657..=677)
+            if retrieved_number < excluding_upper_limit
+                    && is_palindromic(retrieved_number)
+                    && !found_numbers.contains(&retrieved_number) {
+                sum += retrieved_number as i128;
+                found_numbers.insert(retrieved_number);
+                println!("{} by {:?}", retrieved_number, (current_range_start ..= current_range_end));
+            }
+
+            // Abort if current range is already above limit
+            if retrieved_number >= excluding_upper_limit {
+                break;
+            }
+        }
+    }
+
+    println!("Sum: {}", sum);
+}
+
+fn find_palindromic_numbers_with_consecutive_squares_by_number(excluding_upper_limit: i64) {
     let mut sum: i128 = 0;
 
-    for current_number in 1 .. 10000000 {
+    for current_number in 1 .. excluding_upper_limit {
         if is_palindromic(current_number) {
             let consecutive_squares = find_consecutive_squares(current_number);
+
             if consecutive_squares.start() < consecutive_squares.end() {
                 sum += current_number as i128;
-                println!("{} with {:?}", current_number, consecutive_squares);
+                println!("{} by {:?}", current_number, consecutive_squares);
             }
         }
     }
@@ -57,22 +97,3 @@ fn find_consecutive_squares(number: i64) -> RangeInclusive<i64> {
 
     return 1 ..= 0;
 }
-
-// fn find_consecutive_squares_starting_at(number: i64, upperStartingNumber: i64) -> Option<i64> {
-//     let mut lowerEndingNumber: Option<i64> = None;
-
-//     let mut sum = 0;
-//     for i in (1 .. upperStartingNumber + 1).rev() {
-//         sum += i * i;
-
-//         // Return lower ending number of the consecutive squared numbers
-//         if sum == number {
-//             lowerEndingNumber = Some(i as i64);
-//             break;
-//         } else if sum > number {
-//             break;
-//         }
-//     }
-
-//     return lowerEndingNumber;
-// }
